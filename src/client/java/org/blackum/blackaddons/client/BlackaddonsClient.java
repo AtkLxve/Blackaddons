999764bebbb034c87d6449a5b7974571e7d76abc
Artem <Zipmaster2852@gmail.com>
2026-01-20 16:31:05 +0300

    Initial commit

---
+++ src/client/java/org/blackum/blackaddons/client/BlackaddonsClient.java
@@ -0,0 +1,10 @@
+package org.blackum.blackaddons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlackaddonsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/blackum/blackaddons/Blackaddons.java
@@ -0,0 +1,10 @@
+package org.blackum.blackaddons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Blackaddons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
