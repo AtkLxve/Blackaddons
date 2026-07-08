34b81ea88597acc9738e88d59be1577121096ea1
BLACKUM <Zipmaster2852@gmail.com>
2026-07-08 20:35:16 -0300

    fix: fix font shadow displacement on 26.2

--- src/main/java/org/blackum/blackaddons/client/render/BlackaddonsRenderPipelines.java
+++ src/main/java/org/blackum/blackaddons/client/render/BlackaddonsRenderPipelines.java
@@ -64,7 +64,7 @@ public class BlackaddonsRenderPipelines {
             .withVertexBinding(0, DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP)
             .withPrimitiveTopology(PrimitiveTopology.QUADS)
             .withCull(false)
-            .withDepthStencilState(new DepthStencilState(CompareOp.LESS_THAN_OR_EQUAL, true, -1.0F, -10.0F))
+            .withDepthStencilState(new DepthStencilState(CompareOp.GREATER_THAN_OR_EQUAL, true, 1.0F, 10.0F))
             .build());
 
     public static final RenderPipeline VECTOR_TEXT = add(RenderPipeline.builder()
